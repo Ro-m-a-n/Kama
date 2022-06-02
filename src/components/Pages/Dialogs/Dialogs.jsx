b6657bce900f33a5c301abe0bb2201fec521c1b9
roman liutyk <ggyrda18@gmail.com>
2022-06-02 22:14:37 +0300

    props in dialog

--- src/components/Pages/Dialogs/Dialogs.jsx
+++ src/components/Pages/Dialogs/Dialogs.jsx
@@ -1,6 +1,29 @@
 import "./Dialogs.css";
+import { NavLink } from "react-router-dom";
+
+const Dialog = (props) => {
+  return (
+    <div className="Dialogs__item active">
+      <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
+    </div>
+  );
+};
+const Message = (props) => {
+  return <div className="Message active">{props.text}</div>;
+};
 
 const Dialogs = (props) => {
-  return <div> dialog </div>;
+  return (
+    <div className="Dialogs">
+      <div className="Dialogs__items">
+        <Dialog name="Roman" id="1" />
+        <Dialog name="Artem" id="2" />
+      </div>
+      <div className="Messages">
+        <Message text="Hello world" /> 
+        <Message text="Hi there" />
+      </div>
+    </div>
+  );
 };
 export default Dialogs;
--- src/components/Pages/Profile/MyPosts/MyPosts.jsx
+++ src/components/Pages/Profile/MyPosts/MyPosts.jsx
@@ -2,12 +2,20 @@ import "./MyPosts.css";
 import Post from "./Post/Post";
 const MyPosts = () => {
   return (
-    <div className="MyPosts">
-      <div>ava +descr</div>
-      <div>my post</div>
-      <div>new post</div>
-      <Post message="True way of Samurai" likes="0"/>
+    <div className="AddPost">
+      <div> <h3>Add post</h3>
+        <div>
+          <textarea></textarea>
+        </div>
+        <div>
+          <button>Add</button>
+        </div>
+      </div>
+
+      <div className="MyPosts">My posts
+      <Post message="True way of Samurai" likes="0" />
       <Post message="I finaly understood props" likes="99" />
+      </div>
     </div>
   );
 };
