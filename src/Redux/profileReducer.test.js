8316022f6eb848b5aa1c1ab5f14a6190cb36ad4e
roman liutyk <ggyrda18@gmail.com>
2022-09-08 22:14:32 +0300

    89.2

--- src/Redux/profileReducer.test.js
+++ src/Redux/profileReducer.test.js
@@ -1,14 +1,23 @@
-import profileReducer, { addText } from './profileReducer';
+import profileReducer, { addText } from "./profileReducer";
+
+let state = {
+  postsData: [
+    { id: 1, text: "True way of Samurai", likes: 5 },
+    { id: 2, text: "I finaly understood props", likes: 100 },
+  ],
+};
 test("length of posts should be 3", () => {
   let action = addText("Example text");
-  let state = {
-    postsData: [
-      { id: 1, text: "True way of Samurai", likes: 5 },
-      { id: 2, text: "I finaly understood props", likes: 100 },
-    ]
-  };
 
   let newState = profileReducer(state, action);
 
   expect(newState.postsData.length).toBe(3);
+});
+
+test("text of new post should be Example text", () => {
+  let action = addText("Example text");
+
+  let newState = profileReducer(state, action);
+
+  expect(newState.postsData[2].text).toBe('Example text');
 });
