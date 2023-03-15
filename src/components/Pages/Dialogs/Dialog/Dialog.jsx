c9dac1d3f13423f72a3901c3a544b2c02f4c1a9c
roman liutyk <ggyrda18@gmail.com>
2023-03-15 16:37:49 +0200

    img pointer

--- src/components/Pages/Dialogs/Dialog/Dialog.jsx
+++ src/components/Pages/Dialogs/Dialog/Dialog.jsx
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom";
 
 const Dialog = (props) => {
   return (
-    <div className="Dialogs__item active">
-      <img className="Avatar"
-          alt=""
-          src={props.dialogAvatar}
-        ></img>
+    <div className="Dialogs_item active">
+      <NavLink className="Dialog_item__avatar" to={`/messages/${props.id}` }>
+        <img className="Avatar" alt="" src={props.dialogAvatar}></img>
+      </NavLink>
+
       <NavLink to={`/messages/${props.id}`}>{props.name}</NavLink>
     </div>
   );
