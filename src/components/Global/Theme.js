5a778dd81c73ba77cc3cda060fec3f03cf5930f9
roman liutyk <ggyrda18@gmail.com>
2023-03-21 19:44:51 +0200

    themes upd

--- src/components/Global/Theme.js
+++ src/components/Global/Theme.js
@@ -4,7 +4,7 @@ const theme = {
     background: "#ccc;",
     primary: "#9abba0;",
     modes: {
-      green: {
+      light: {
         background: "#faf9ed;",
         primary: "#e2dfd7;",
       },
--- src/components/Pages/Settings/Settings.jsx
+++ src/components/Pages/Settings/Settings.jsx
@@ -1,16 +1,33 @@
 import "./Settings.css";
 import { useColorMode } from "theme-ui";
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 /**@jsxImportSource theme-ui */
 const Settings = (props) => {
-  const [ColorMode, setColorMode] = useColorMode();
-  const [themesPositionScaleLight, setThemesPositionScaleLight] = useState(1);
-  const [themesPositionScaleGreen, setThemesPositionScaleGreen] = useState(1.2);
-  // const [themesPosition, setThemesPosition] = useState("-20%");
+  const [ColorMode, setColorMode] = useColorMode("green");
+  const [themesPositionScaleLight, setThemesPositionScaleLight] = useState();
+  const [themesPositionScaleGreen, setThemesPositionScaleGreen] = useState();
   const [themesBorderColorLight, setThemesBorderColorLight] =
     useState("#b8f7c4");
   const [themesBorderColorGreen, setThemesBorderColorGreen] = useState("green");
+
+  useEffect(() => {
+    if (ColorMode === "green") {
+      return (
+        setThemesPositionScaleLight(1),
+        setThemesPositionScaleGreen(1.2),
+        setThemesBorderColorLight("#b8f7c4"),
+        setThemesBorderColorGreen("green")
+      );
+    } else {
+      return (
+        setThemesPositionScaleLight(1.2),
+        setThemesPositionScaleGreen(1),
+        setThemesBorderColorLight("green"),
+        setThemesBorderColorGreen("#b8f7c4")
+      );
+    }
+  }, [ColorMode]);
 
   return (
     <div className="settings_wrap" sx={{ bg: "primary" }}>
@@ -18,19 +35,9 @@ const Settings = (props) => {
       <div className="themes">
         <motion.div
           className="themes_light"
-          onClick={() => {
-            return (
-              setColorMode("green"),
-              //  setThemesPosition("-20%"),
-              setThemesBorderColorLight("green"),
-              setThemesBorderColorGreen("#b8f7c4"),
-              setThemesPositionScaleLight(1.2),
-              setThemesPositionScaleGreen(1)
-            );
-          }}
+          onClick={() => setColorMode("light")}
           whileHover={{ scale: 1.2, duration: 0.5 }}
           animate={{
-            // x: themesPosition,
             scale: themesPositionScaleLight,
             borderColor: themesBorderColorLight,
           }}
@@ -40,19 +47,9 @@ const Settings = (props) => {
         </motion.div>
         <motion.div
           className="themes_green"
-          onClick={() => {
-            return (
-              setColorMode("light"),
-              //  setThemesPosition("20%"),
-              setThemesBorderColorLight("#b8f7c4"),
-              setThemesBorderColorGreen("green"),
-              setThemesPositionScaleLight(1),
-              setThemesPositionScaleGreen(1.2)
-            );
-          }}
+          onClick={() => setColorMode("green")}
           whileHover={{ scale: 1.2, duration: 0.5 }}
           animate={{
-            // x: themesPosition,
             scale: themesPositionScaleGreen,
             borderColor: themesBorderColorGreen,
           }}
