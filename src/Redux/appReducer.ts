eb0669e4345afd421129fd234ee0fdfaa40122af
roman liutyk <ggyrda18@gmail.com>
2023-03-28 13:50:10 +0300

    app reducer

--- src/Redux/appReducer.ts
+++ src/Redux/appReducer.ts
@@ -1,13 +1,13 @@
-
 import { isLoginedTC } from "./authReducer";
 
 const INITIALIZED_SUCCES = "INITIALIZED_SUCCES";
 
+type InitialStateType = typeof initialState;
 let initialState = {
   initialized: false,
 };
 
-let appReducer = (state = initialState, action) => {
+let appReducer = (state: InitialStateType = initialState, action: any) => {
   switch (action.type) {
     case INITIALIZED_SUCCES:
       return {
@@ -19,11 +19,15 @@ let appReducer = (state = initialState, action) => {
       return state;
   }
 };
-export const initializedSucces = () => ({
+type InitializedSuccesType = {
+  type: typeof INITIALIZED_SUCCES;
+};
+
+export const initializedSucces = (): InitializedSuccesType => ({
   type: INITIALIZED_SUCCES,
 });
 
-export const initializeAppTC = () => (dispatch) => {
+export const initializeAppTC = () => (dispatch: any) => {
   let promise = dispatch(isLoginedTC());
   Promise.all([promise]).then(() => {
     dispatch(initializedSucces());
