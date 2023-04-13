1f067a0d1f9e1ad944eeb35ed50484a4d6450039
roman liutyk <ggyrda18@gmail.com>
2023-04-13 12:24:41 +0300

    upd hash

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import store from "./Redux/reduxStore";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { ThemeProvider } from "theme-ui";
 import theme from "./components/Global/Theme";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <App />
       </Provider>
     </ThemeProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
